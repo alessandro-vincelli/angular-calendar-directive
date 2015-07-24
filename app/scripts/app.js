10a2c6107aced23eba513ef37dc58f8c07fe6b1d
Ashish Gupta <ashishgupta1989@gmail.com>
2015-07-24 12:18:31 +0530

    Pushing Demo on github Pages

--- app/scripts/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by synerzip on 09/07/15.
- */
-angular.module("app",["ui-calendar"])
-.controller("appController",["$scope",function($scope){
-       var today = moment();
-        $scope.day =  today;
-    }]);
